8276f615017a51b6df051cffbfb7a1b05c92b969
Emil <40684650+MrCheatEugene@users.noreply.github.com>
2024-04-28 23:50:47 +0300

    Update tests.py

--- pyCryptoPayAPI/tests.py
+++ pyCryptoPayAPI/tests.py
@@ -2,9 +2,9 @@ import inspect, datetime
 import asyncio
 from time import sleep
 try:
-    from pyCryptoPayAPI import pyCryptoPayAPI, pyCryptoPayException
+    from aCryptoPayAPI import aCryptoPayAPI, aCryptoPayException
 except:
-    from api import pyCryptoPayAPI, pyCryptoPayException 
+    from api import aCryptoPayAPI, aCryptoPayException 
 
 test_api_token = "52586:AA2DKQyUAnZFELNOihEqjlfact0XsxoUmGy"
 
